d1d16799e240c07dd68e905b4ee731224504700b
nunulk <nunulk@usingstd.com>
2019-10-09 22:02:10 +0900

    Fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'nunulk\'s blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
